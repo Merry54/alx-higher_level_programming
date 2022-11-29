b4b10814fd1c53ed8c436fa938dde809979c3251
root <merry54>
2022-11-29 18:41:24 +0000

    high level

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/phython3
+for i in range(100):
+if int(i / 10) != i % 10 and int(i / 10) < i % 10:
+print("{}{}".format(int(i / 10), i % 10), end="")
+f (i != 89):
+print(", ", end="")
+
+print("")
