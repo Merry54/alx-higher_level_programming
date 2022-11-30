d742696ea6f5634a63d440aa452ce4db0e68e87a
merahawit <111182577+Merry54@users.noreply.github.com>
2022-11-30 22:39:45 +0300

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/phyton3                                                            
-for i in range(100):                                                            if i == 99:                                                                     
+for i in range(100):                                                           
+if i == 99:                                                                     
 print(i)                                                                        
 else:                                                                           
 print("{}".format('0' + str(i) if i < 10 else i), end=", ")
