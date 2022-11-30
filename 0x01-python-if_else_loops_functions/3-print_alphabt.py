119da55375c93080bf87af7385f6185a9163e8cf
merahawit <111182577+Merry54@users.noreply.github.com>
2022-11-30 22:39:00 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/phython3
 char in range(26):                                                                                                                  
-if char != 4 and char != 16:                                                    print("{:s}".format(chr(char + ord("a"))), end="")v
+if char != 4 and char != 16:                                                   
+print("{:s}".format(chr(char + ord("a"))), end="")
