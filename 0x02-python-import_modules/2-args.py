2ba07db926407719ff1513fc61abfa5d9dd04bf6
root <merry54>
2022-12-01 14:33:15 +0000

    all

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
-length = len(argv)
-# Only run if module is executed as main file
+
 if __name__ == "__main__":
-    # Argument index starts from 0 while its length from 1
-    if length == 2:
-        print("{} argument:".format(1))
-        print("{}: {}".format(1, argv[1]))
-    elif length == 1:
-        print("{} arguments.".format(0))
-    elif length > 2:
-        print("{} arguments:".format(length - 1))
-        for i in range(1, length):
-            print("{}: {}".format(i, argv[i]))
+
+    """Print the number of and list of arguments."""
+    import sys
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
