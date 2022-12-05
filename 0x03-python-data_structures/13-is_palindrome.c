43e4a828d7f6db445d908f6f94f24ab098a4f0fd
root <merry54>
2022-12-05 20:42:01 +0000

    13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,9 +5,7 @@
  * @head: pointer to the first node in the list
  * Return: pointer to the first node in the new list
  */
-
 void reverse_listint(listint_t **head)
-
 {
 	listint_t *prev = NULL;
 	listint_t *current = *head;
@@ -32,41 +30,12 @@ void reverse_listint(listint_t **head)
  */
 
 int is_palindrome(listint_t **head)
-
 {
 	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	while (1)
-	{
-		fast = fast->next->next;
-		if (!fast)
-		{
-			dup = slow->next;
-			break;
-		}
-		if (!fast->next)
-		{
-			dup = slow->next->next;
-			break;
-		}
-		slow = slow->next;
-	}
-	reverse_listint(&dup);
-	while (dup && temp)
-	{
-		if (temp->n == dup->n)
-		{
-			dup = dup->next:
-			temp = temp->next;
-		}
-		else
-			return (0);
-	}
-	if (!dup)
-		return (1);
-	return (0);
-}
+
 while (1)
 	{
 		fast = fast->next->next;
@@ -83,6 +52,7 @@ while (1)
 		slow = slow->next;
 	}
 	reverse_listint(&dup);
+
 	while (dup && temp)
 	{
 		if (temp->n == dup->n)
