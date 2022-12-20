8fa77aa7edf6a70cf565610267a1fa2f2135ae83
root <merry54>
2022-12-20 22:10:21 +0000

    add

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,43 +1,45 @@
 #!/usr/bin/python3
+
 """module for a singly linked list"""
 
 
 class Node:
     """"defines a node"""
 
-def __init__(self, data, next_node=None):
+    def __init__(self, data, next_node=None):
         """initializes the node with instance variables"""
 
-  self.data = data
+        self.data = data
         self.next_node = next_node
 
-  @property
+    @property
     def data(self):
         """gets data attribute"""
 
- return (self.__data)
+        return (self.__data)
 
- @data.setter
-    def data(self, value)
+    @data.setter
+    def data(self, value):
         """sets data attribute"""
 
- if not isinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError('data must be an integer')
         self.__data = value
 
-  @property
+    @property
     def next_node(self):
         """get next_node attribute
         Returns: next node
         """
 
-return (self.__next_node)
-@next_node.setter
+        return (self.__next_node)
+
+    @next_node.setter
     def next_node(self, value):
         """set value of next node"""
 
         if (value is not None and not isinstance(value, Node)):
-            raise TypeError('net_node must be a Node object')
+            raise TypeError('next_node must be a Node object')
 
         self.__next_node = value
 
@@ -59,7 +61,6 @@ class SinglyLinkedList:
             printsll += str(location.data) + "\n"
             location = location.next_node
         return printsll[:-1]
-
     def sorted_insert(self, value):
         """insert in a sorted fashion
         Args:
@@ -69,7 +70,7 @@ class SinglyLinkedList:
         if not self.head:
             self.head = new
             return
- if value < self.head.data:
+        if value < self.head.data:
             new.next_node = self.head
             self.head = new
             return
